d8d3cbd5263795d2be26bb7a71cd88f898c92b15
Guan-Tse Wu <guan-tse.wu@griffithuni.edu.au>
2022-04-06 13:51:20 +1000

    Components done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import { useState } from "react";
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, text: "Doctors Appointment", day: "Feb 5th 2pm", reminder: true },
+    { id: 2, text: "Meeting", day: "Feb 6th 2pm", reminder: true },
+  ]);
+  //add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+  //delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+  //toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <AddTask onAdd={addTask} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No tasks to show"
+      )}
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Button from "./Button";
+const Header = ({ title }) => {
+  return (
+    <header>
+      <h1>{title}</h1>
+      <Button text="Add" />
+    </header>
+  );
+};
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default Header;
